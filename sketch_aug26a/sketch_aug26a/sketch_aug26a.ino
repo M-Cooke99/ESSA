6af6874d944a53532014316a200ade3f0d7822a3
M-Cooke99 <Sc2coomich@gmail.com>
2024-08-26 18:45:44 +1000

    32Hz clock

--- sketch_aug26a/sketch_aug26a/sketch_aug26a.ino
+++ sketch_aug26a/sketch_aug26a/sketch_aug26a.ino
@@ -1,25 +1,26 @@
 int timer = 0;
 uint16_t data = 0;
 
-#define TimerHz 50
+#define TimerHz 32
 
 void setup() {
     // Timer
     TCCR1A = 0;
     TCCR1B = 0;
     TCNT1 = 0; // Start at 0
-    TCCR1B |= (1 << CS12) | (0 << CS11) |  (0 << CS10); // 256 Prescale
+    TCCR1B |= (0 << CS12) | (1 << CS11) |  (0 << CS10); // 8 Prescale
     TCCR1B |= (1 << WGM12); // Waveform from TCCR1A
     TIMSK1 |= (1 << OCIE1A); //16,000,000 Hz timer
-    #if (625000 % TimerHz != 0)
+    #if (2000000 % TimerHz != 0)
     #error Invalid TimerHz
     #endif
-    OCR1A = (62500 / TimerHz) - 1; //Count before interrupt
+    OCR1A = (2000000 / TimerHz) - 1; //Count before interrupt
 
     Serial.begin(250000);
 }
 
 ISR(TIMER1_COMPA_vect){
+    Serial.println(timer);
     if (timer >= TimerHz - 1) {
         timer = 0;
     } else {
